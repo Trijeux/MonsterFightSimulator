f2288fe1928abbf8eac96fb0c4f9d992e804469c
Trijeux <anthonybarman.2003@gmail.com>
2023-10-13 12:48:42 +0200

    Add choise Race au Player

--- MonsterFightSimulator.cpp
+++ MonsterFightSimulator.cpp
@@ -4,56 +4,77 @@
 
 int main()
 {
-	Monster monster1(100, 30, 20, 10, Orc);
-	Monster monster2(100, 30, 20, 10, Troll);
-	Monster Rounder(0, 0, 0);
-
-	//monster1.Attack(monster2);
-	//monster2.Attack(monster1);
-
-	bool EndBattle = true;
-	int priority;
+	bool gameContinue = true;
+	std::cout << "Bonjour, bienvenu dans Monster Fight Simulateur" << std::endl;
 	do
 	{
-		std::cout << "---------------------" << std::endl;
-		Rounder.Round();
-		if (monster1.getS() == monster2.getS())
+		char playerInputContinue;
+		int joueurChoise1;
+		int joueurChoise2;
+		std::cout << "Veuillez choisire la Race du premier monstre Orc(0), Troll(1) ou Goblin(2) :" << std::endl;
+		std::cin >> joueurChoise1;
+		std::cout << "ET le deuxieme Orc(0), Troll(1) ou Goblin(2) :";
+		std::cin >> joueurChoise2;
+		system("cls");
+
+		Monster monster1(100, 30, 20, 10, Race(joueurChoise1));
+		Monster monster2(100, 30, 20, 10, Race(joueurChoise2));
+		Monster Rounder(0, 0, 0);
+
+		//monster1.Attack(monster2);
+		//monster2.Attack(monster1);
+
+		bool EndBattle = true;
+		int priority;
+		do
 		{
-			std::random_device rand;
-			std::default_random_engine e2(rand());
-			std::uniform_int_distribution<> nbrand(1, 2);
-			priority = nbrand(e2);
-		}
-		if (monster1.getS() > monster2.getS() || priority == 1)
-		{
-			monster1.State();
-			monster1.Choix(monster2);
-			EndBattle = Rounder.DeadOrNot(monster1, monster2);
-			if (EndBattle)
+			std::cout << "---------------------" << std::endl;
+			Rounder.Round();
+			if (monster1.getS() == monster2.getS())
 			{
-				monster2.State();
-				monster2.Choix(monster1);
+				std::random_device rand;
+				std::default_random_engine e2(rand());
+				std::uniform_int_distribution<> nbrand(1, 2);
+				priority = nbrand(e2);
 			}
-		}
-		if (monster1.getS() < monster2.getS() || priority == 2)
-		{
-			monster2.State();
-			monster2.Choix(monster1);
-			EndBattle = Rounder.DeadOrNot(monster1, monster2);
-			if (EndBattle)
+			if (monster1.getS() > monster2.getS() || priority == 1)
 			{
 				monster1.State();
 				monster1.Choix(monster2);
+				EndBattle = Rounder.DeadOrNot(monster1, monster2);
+				if (EndBattle)
+				{
+					monster2.State();
+					monster2.Choix(monster1);
+				}
 			}
-		}
-	/*	std::cout << "Apres action avant reset" << std::endl;
+			if (monster1.getS() < monster2.getS() || priority == 2)
+			{
+				monster2.State();
+				monster2.Choix(monster1);
+				EndBattle = Rounder.DeadOrNot(monster1, monster2);
+				if (EndBattle)
+				{
+					monster1.State();
+					monster1.Choix(monster2);
+				}
+			}
+			/*	std::cout << "Apres action avant reset" << std::endl;
 		monster1.State();
 		std::cout << "Apres action avant reset" << std::endl;
 		monster2.State();*/
-		Rounder.EndOfRound(monster1, monster2);
-		if (EndBattle)
+			Rounder.EndOfRound(monster1, monster2);
+			if (EndBattle)
+			{
+				EndBattle = Rounder.DeadOrNot(monster1, monster2);
+			}
+		} while (EndBattle);
+		std::cout << "Veux tu Recomencer oui(y) ou non(n) : ";
+		std::cin >> playerInputContinue;
+		if (playerInputContinue == 'n')
 		{
-			EndBattle = Rounder.DeadOrNot(monster1, monster2);
+			gameContinue = false;
 		}
-	} while (EndBattle);
+	}
+	while (gameContinue);
 }
